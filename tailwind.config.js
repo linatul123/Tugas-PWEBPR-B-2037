df8773c0f24f273f5b63ca9e0abf293d76f9c4ac
Lina <linatulhabibah18@gmail.com>
2024-09-25 22:14:55 +0700

    upload file

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors : {
+        bgLuffy : "#FFF4DC",
+        buttonLuffy : '#E29A00',
+        bgZoro : "#E1FFDC",
+        buttonZoro : "#7EB782",
+        bgNami : "#FFF4DC",
+        buttonNami : "#FF5573",
+        bgSanji : "#E2F2FF",
+        buttonSanji : "#3279AF",
+        bgUsop : "#FFFFFF",
+        buttonUsop : "#D8D6C1",
+        bgChopper : "#FFFFFF",
+        buttonChopper :"#D8D6C1",
+        bgRobin : "#FFFFFF",
+        buttonRobin : "#D8D6C1",
+        bgBrook : "#F2F2F2",
+        butoonBrook : "#D8D6C1",
+        bgJinbe : "#F2F2F2",
+        butoonJinbe : "#D8D6C1",
+      },
+      fontFamily : {
+        'sans' : ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
+
